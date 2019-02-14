3fa7490ccf341f69a020a944cdc8b1023b642581
Adam Urbina <adamurbina@gmail.com>
2019-02-14 01:15:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function (block1, block2) {
+
     const blocks = parseInt(block2.substring(0,block2.length-2)) - parseInt(block1.substring(0,block1.length-2))
 
     if (blocks > blockRange) {
@@ -9,4 +10,8 @@ function produceDrivingRange(blockRange) {
       return `within range by ${blockRange - blocks}`
     }
   };
+}
+
+function produceTipCalculator() {
+  
 }
