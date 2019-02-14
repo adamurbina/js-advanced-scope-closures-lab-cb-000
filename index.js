9057961e61b33f48d715f103721ac9f7dc7c1eef
Adam Urbina <adamurbina@gmail.com>
2019-02-14 01:14:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function (block1, block2) {
-    const blocks = parseInt(block1.substring(0,block1.length-2)) - parseInt(block2.substring(0,block2.length-2))
+    const blocks = parseInt(block2.substring(0,block2.length-2)) - parseInt(block1.substring(0,block1.length-2))
 
     if (blocks > blockRange) {
       return `${blocks - blockRange} blocks out of range`
