dbc22859a3f5a7bcd91761abc38d267f206addff
Adam Urbina <adamurbina@gmail.com>
2019-02-14 00:45:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function produceDrivingRange(blockRange) {
   return function withinRange(bound) {
-    return bound < blockRange;
-  }
+    if (bound > blockRange) {
+      return `${bound - blockRange} blocks out of range`
+    }
+    else{
+      return `within range by ${blockRange - block}`
+    }
+  };
 }
