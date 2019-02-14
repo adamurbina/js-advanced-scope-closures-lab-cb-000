c31644f7f86334e6ab9afa2e903fd2d89f211bf9
Adam Urbina <adamurbina@gmail.com>
2019-02-14 00:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(blockRange) {
-  return function (bound) {
-    if (bound > blockRange) {
-      return `${bound - blockRange} blocks out of range`
+  return function (block) {
+    if (block > blockRange) {
+      return `${block - blockRange} blocks out of range`
     }
     else{
       return `within range by ${blockRange - block}`
